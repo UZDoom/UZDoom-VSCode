1c2f852fc538c16b6c180cdde50eb16ee3428796
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-06-08 15:56:34 -0700

    fix setting name for Lump

--- src/doom-wad/Lumps/Lump.ts
+++ src/doom-wad/Lumps/Lump.ts
@@ -1,31 +1,33 @@
 export default class Lump
 {
-    private _index: number = 0;
+	private _index: number = 0;
 	private _name: string = "";
 	private _content: ArrayBuffer = new ArrayBuffer(0);
 
-    static isValidName(name: string): boolean {
-        return /^[A-Z0-9\[\]\-_\\]+$/.test(name);
-    }
+	static isValidName(name: string): boolean {
+		// return /^[A-Z0-9\[\]\-_\\]+$/.test(name);
+		// the above test is incorrect; doom allows all printable ascii characters
+		return /^[\x20-\x7E]+$/.test(name);
+	}
 
-    static sanitizeName(name: string): string {
-        name = name.trim().replace(/\0+$/g, "");
-        if (name.startsWith("\\")) {
-            name = name.substring(1);
-        }
-        if (name.endsWith("**")) {
-            name = name.substring(0, name.length - 2);
-        }
-        return name;
-    }
 
-    get index(): number {
-        return this._index;
-    }
+	static trimName(name: string): string {
+		return name.trim().replace(/\0+$/g, "");
+	}
 
-    set index(value: number) {
-        this._index = value;
-    }
+	static sanitizeName(name: string): string {
+		let result = Lump.trimName(name);
+		result = result.replace(/[^\x20-\x7E]/g, "_");
+		return result;
+	}
+
+	get index(): number {
+		return this._index;
+	}
+
+	set index(value: number) {
+		this._index = value;
+	}
 
 	get name(): string
 	{
@@ -34,20 +36,17 @@ export default class Lump
 
 	set name(value: string)
 	{
-        value = Lump.sanitizeName(value);
-        if (!Lump.isValidName(value)) {
-            console.warn(`Invalid lump name ${value}`);
-            return;
-        }
-
+		value = Lump.trimName(value);
 		if(value.length > 8)
-        {
+		{
 			console.warn(`Name ${value} will be truncated`);
-
 			value = value.substring(0, 8);
 		}
-
-		this._name = value + Array( 8 - value.length + 1 ).join("\x00");
+		this._name = value;
+		if (!Lump.isValidName(value)) {
+			this._name = Lump.sanitizeName(value);
+			console.warn(`Invalid lump name ${value}, renaming to ${this._name}`);
+        }
 	}
 
 	get content(): ArrayBuffer
@@ -60,18 +59,18 @@ export default class Lump
 		this._content = value;
 	}
 
-    get rawContent(): ArrayBuffer {
-        return this._content;
-    }
+	get rawContent(): ArrayBuffer {
+		return this._content;
+	}
 
 	get length(): number
 	{
-        return this._content.byteLength; // NB: Add terminator
-    }
+		return this._content.byteLength; // NB: Add terminator
+	}
 
-    constructor(index: number, name: string, content: ArrayBuffer) {
-        this.index = index;
-        this.name = name;
-        this.content = content;
-    }
+	constructor(index: number, name: string, content: ArrayBuffer) {
+		this.index = index;
+		this.name = name;
+		this.content = content;
+	}
 }
